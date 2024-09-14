e47caa384b5a66c01a229359cbe0a356fd9aa1ed
gerumh.lincoln <qraztec@gmail.com>
2024-09-14 15:17:26 -0700

    Added enter

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
+//const port = 9000;
 
 const db = require('knex')({
     client: 'pg',
     connection: {
-      connectionString: process.env.DATABASE_URL,
+      connectionString: "postgresql://hydration_data_cuem_user:AzwRb0OHkxEyNa0keMbtEymNACtcgcUp@dpg-criui3e8ii6s73fal8p0-a.virginia-postgres.render.com/hydration_data_cuem",
       ssl: { rejectUnauthorized: false },
-      host: process.env.DATABASE_HOST,
+      host: "dpg-criui3e8ii6s73fal8p0-a",
       port: 5432,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PW,
-      database: process.env.DATABASE_DB,
+      user: "hydration_data_cuem_user",
+      password: "AzwRb0OHkxEyNa0keMbtEymNACtcgcUp",
+      database: "hydration_data_cuem",
     },
   });
 
@@ -19,7 +20,6 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
 // Start the server
 app.listen(8000, () => {
     console.log('app is running on port 8000')
--- controllers/enter.js
+++ controllers/enter.js
@@ -1,22 +1,28 @@
-const handleEnter = (req, res, db) => {
+const handleEnter = async(req, res, db) => {
     const {name,bottlesCleaned, soapLevel, sanitation, numBottlesFilled} = req.body;
     if (!name || !bottlesCleaned || !soapLevel || !sanitation || !numBottlesFilled) {
         return res.status(400).json('incorrect for submission')
     }
-    
-        db.transaction(trx => {
-            console.log(soapLevel)
-            trx.insert({
+    try {
+        console.log(name)
+        await db.transaction(async (trx) => {
+            
+            await trx('sensors').insert({
                 name: name,
-                bottlesCleaned: bottlesCleaned,
-                soapLevel: soapLevel,
+                bottlescleaned: bottlesCleaned,
+                soaplevel: soapLevel,
                 sanitation: sanitation,
-                numBottlesFilled: numBottlesFilled
+                numbottlesfilled: numBottlesFilled
             })
-            .into('sensors')
-            .then(trx.commit)
-            .catch(trx.rollback)
-    }).catch(err => res.status(400).json('unable to enter'))
+        })
+        res.status(200).json("successfully entered")
+    }
+    catch (err) {
+        console.error('Database transaction error:', err);
+        res.status(500).json('unable to enter into database')
+    }
+        
+    
     
 
 }
