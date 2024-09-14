c612ec97e25d22ae5f31e8706228134f7b5d98c5
gerumh.lincoln <qraztec@gmail.com>
2024-09-14 13:37:41 -0700

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-require('dotenv').config()
-// const {Pool} = require('pg')
+
 const db = require('knex')({
     client: 'pg',
     connection: {
@@ -15,39 +13,15 @@ const db = require('knex')({
       database: process.env.DATABASE_DB,
     },
   });
-//   const pool = new Pool({
-//     user: process.env.DATABASE_USER,
-//     host: process.env.DATABASE_HOST,
-//     database: process.env.DATABASE_DB,
-//     password: process.env.DATABASE_PW,
-//     port: 5432,
-//     ssl : {rejectUnauthorized: false}
-//   })
 
-//   async function testConnection() {
-//     console.log(process.env.DATABASE_USER)
-//     try {
-//       await pool.connect();  // tries to get a connection from the pool
-//       console.log('Connected to the database successfully!');
-//     } catch (err) {
-//       console.log('Database connection failed:');
-//     } finally {
-//       await pool.end();  // closes all active connections once they return to the pool
-//     }
-//   }
-// testConnection()
-// Middleware to parse JSON requests
 app.use(express.json());
 
-// Define a simple route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`API running on http://localhost:${port}`);
-});
+
 
 const enter = require('./controllers/enter');
 
